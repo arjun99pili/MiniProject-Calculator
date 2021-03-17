575551117ad12bca27028fda402b35b2792442ac
Arjun Pilikudale <arjunpilikudale@gmail.com>
2021-03-17 16:46:16 +0530

    Initiated Ansible inventory

--- src/test/java/calc/CalcTester.java
+++ src/test/java/calc/CalcTester.java
@@ -20,6 +20,63 @@ public class CalcTester
         double res = testCalc.findRoot(x);
 
         Assert.assertEquals(answer, res, 1);
+
     }
+
+    @Test
+    public void testingFactorial()
+    {
+        double x = 3;
+        double answer = 6;
+        double res = testCalc.findFactorial(x);
+
+        Assert.assertEquals(answer, res, 1);
+
+    }
+
+    @Test
+    public void wrongInputFactorial()
+    {
+        double x = 3.3;
+        double answer = -1;
+        double res = testCalc.findFactorial(x);
+
+        Assert.assertEquals(answer, res, 1);
+
+    }
+
+    @Test
+    public void testingLog()
+    {
+        double x = 3;
+        double answer = 1;
+        double res = testCalc.findLog(x);
+
+        Assert.assertEquals(answer, res, 1);
+    }
+
+    @Test
+    public void testingExpo()
+    {
+        double x = 3;
+        double b = 2;
+        double answer = 9;
+        double res = testCalc.findExpo(x, b);
+
+        Assert.assertEquals(answer, res, 1);
+    }
+
+    @Test
+    public void wrongInputExpo()
+    {
+        double x = 3;
+        double b = 2.2;
+        double answer = -1;
+        double res = testCalc.findExpo(x, b);
+
+        Assert.assertEquals(answer, res, 1);
+    }
+
+
 }
 
